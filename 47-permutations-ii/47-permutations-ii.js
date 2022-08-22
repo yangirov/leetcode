399de6f46b194dcc4d1b523ce6497a5743f45075
Emil Yangirov <32175431+yangirov@users.noreply.github.com>
2022-08-22 17:10:05 +0500

    Time: 95 ms (82.95%), Space: 45.9 MB (56.47%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    let result = []
+    
+    const dfs = (i, nums) => {
+        if (i == nums.length) {
+            result.push(nums.slice())
+            return
+        }
+        
+        let hash = {}
+        
+        for (let j=i; j < nums.length; j++) {
+            if (hash[nums[j]])
+                continue;
+            
+            hash[nums[j]] = true;
+            
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+            dfs(i+1, nums);
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+        }
+    }
+    
+    dfs(0, nums)
+    return result
+};
